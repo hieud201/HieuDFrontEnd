fe5db69381d8980f82483b910f82898d9f296835
hieud201 <hieud201@uw.edu>
2024-11-26 16:43:21 -0800

    fixed prettier problems

--- src/sections/auth/auth-forms/AuthLogin.tsx
+++ src/sections/auth/auth-forms/AuthLogin.tsx
@@ -57,151 +57,149 @@ export default function AuthLogin({ providers, csrfToken }: any) {
   };
 
   return (
-    <>
-      <Formik
-        initialValues={{
-          email: 'Hattie.Hyatt28@yahoo.com',
-          password: 'ads12345',
-          submit: null
-        }}
-        validationSchema={Yup.object().shape({
-          email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-          password: Yup.string()
-            .required('Password is required')
-            .test('no-leading-trailing-whitespace', 'Password cannot start or end with spaces', (value) => value === value.trim())
-            .max(10, 'Password must be less than 10 characters')
-        })}
-        onSubmit={(values, { setErrors, setSubmitting }) => {
-          const trimmedEmail = values.email.trim();
-          signIn('login', {
-            redirect: false,
-            email: trimmedEmail,
-            password: values.password,
-            callbackUrl: APP_DEFAULT_PATH
-          }).then(
-            (res: any) => {
-              if (res?.error) {
-                setErrors({ submit: res.error });
-                setSubmitting(false);
-              } else {
-                preload('api/menu/dashboard', fetcher);
-                setSubmitting(false);
-              }
-            },
-            (res) => {
+    <Formik
+      initialValues={{
+        email: 'Hattie.Hyatt28@yahoo.com',
+        password: 'ads12345',
+        submit: null
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+        password: Yup.string()
+          .required('Password is required')
+          .test('no-leading-trailing-whitespace', 'Password cannot start or end with spaces', (value) => value === value.trim())
+          .max(10, 'Password must be less than 10 characters')
+      })}
+      onSubmit={(values, { setErrors, setSubmitting }) => {
+        const trimmedEmail = values.email.trim();
+        signIn('login', {
+          redirect: false,
+          email: trimmedEmail,
+          password: values.password,
+          callbackUrl: APP_DEFAULT_PATH
+        }).then(
+          (res: any) => {
+            if (res?.error) {
               setErrors({ submit: res.error });
               setSubmitting(false);
+            } else {
+              preload('api/menu/dashboard', fetcher);
+              setSubmitting(false);
             }
-          );
-        }}
-      >
-        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-          <form noValidate onSubmit={handleSubmit}>
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="email-login">Email Address</InputLabel>
-                  <OutlinedInput
-                    id="email-login"
-                    type="email"
-                    value={values.email}
-                    name="email"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Enter email address"
-                    fullWidth
-                    error={Boolean(touched.email && errors.email)}
-                  />
-                </Stack>
-                {touched.email && errors.email && (
-                  <FormHelperText error id="standard-weight-helper-text-email-login">
-                    {errors.email}
-                  </FormHelperText>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="password-login">Password</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    color={capsWarning ? 'warning' : 'primary'}
-                    error={Boolean(touched.password && errors.password)}
-                    id="-password-login"
-                    type={showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    name="password"
-                    onBlur={(event: FocusEvent<any, Element>) => {
-                      setCapsWarning(false);
-                      handleBlur(event);
-                    }}
-                    onKeyDown={onKeyDown}
-                    onChange={handleChange}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                          color="secondary"
-                        >
-                          {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    placeholder="Enter password"
-                  />
-                  {capsWarning && (
-                    <Typography variant="caption" sx={{ color: 'warning.main' }} id="warning-helper-text-password-login">
-                      Caps lock on!
-                    </Typography>
-                  )}
-                </Stack>
-                {touched.password && errors.password && (
-                  <FormHelperText error id="standard-weight-helper-text-password-login">
-                    {errors.password}
-                  </FormHelperText>
-                )}
-              </Grid>
-
-              <Grid item xs={12} sx={{ mt: -1 }}>
-                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={checked}
-                        onChange={(event) => setChecked(event.target.checked)}
-                        name="checked"
-                        color="primary"
-                        size="small"
-                      />
-                    }
-                    label={<Typography variant="h6">Keep me sign in</Typography>}
-                  />
-                  <NextLink href={'/forget-pass'} passHref legacyBehavior>
-                    <Link variant="h6" color="text.primary">
-                      Forgot Password?
-                    </Link>
-                  </NextLink>
-                </Stack>
-              </Grid>
-              {errors.submit && (
-                <Grid item xs={12}>
-                  <FormHelperText error>{errors.submit}</FormHelperText>
-                </Grid>
+          },
+          (res) => {
+            setErrors({ submit: res.error });
+            setSubmitting(false);
+          }
+        );
+      }}
+    >
+      {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+        <form noValidate onSubmit={handleSubmit}>
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Stack spacing={1}>
+                <InputLabel htmlFor="email-login">Email Address</InputLabel>
+                <OutlinedInput
+                  id="email-login"
+                  type="email"
+                  value={values.email}
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Enter email address"
+                  fullWidth
+                  error={Boolean(touched.email && errors.email)}
+                />
+              </Stack>
+              {touched.email && errors.email && (
+                <FormHelperText error id="standard-weight-helper-text-email-login">
+                  {errors.email}
+                </FormHelperText>
               )}
-              <Grid item xs={12}>
-                <AnimateButton>
-                  <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
-                    Login
-                  </Button>
-                </AnimateButton>
-              </Grid>
             </Grid>
-          </form>
-        )}
-      </Formik>
-    </>
+            <Grid item xs={12}>
+              <Stack spacing={1}>
+                <InputLabel htmlFor="password-login">Password</InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  color={capsWarning ? 'warning' : 'primary'}
+                  error={Boolean(touched.password && errors.password)}
+                  id="-password-login"
+                  type={showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  name="password"
+                  onBlur={(event: FocusEvent<any, Element>) => {
+                    setCapsWarning(false);
+                    handleBlur(event);
+                  }}
+                  onKeyDown={onKeyDown}
+                  onChange={handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        color="secondary"
+                      >
+                        {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  placeholder="Enter password"
+                />
+                {capsWarning && (
+                  <Typography variant="caption" sx={{ color: 'warning.main' }} id="warning-helper-text-password-login">
+                    Caps lock on!
+                  </Typography>
+                )}
+              </Stack>
+              {touched.password && errors.password && (
+                <FormHelperText error id="standard-weight-helper-text-password-login">
+                  {errors.password}
+                </FormHelperText>
+              )}
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: -1 }}>
+              <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={checked}
+                      onChange={(event) => setChecked(event.target.checked)}
+                      name="checked"
+                      color="primary"
+                      size="small"
+                    />
+                  }
+                  label={<Typography variant="h6">Keep me sign in</Typography>}
+                />
+                <NextLink href={'/forget-pass'} passHref legacyBehavior>
+                  <Link variant="h6" color="text.primary">
+                    Forgot Password?
+                  </Link>
+                </NextLink>
+              </Stack>
+            </Grid>
+            {errors.submit && (
+              <Grid item xs={12}>
+                <FormHelperText error>{errors.submit}</FormHelperText>
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <AnimateButton>
+                <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                  Login
+                </Button>
+              </AnimateButton>
+            </Grid>
+          </Grid>
+        </form>
+      )}
+    </Formik>
   );
 }
--- src/sections/messages/message-forms/messageSend.tsx
+++ src/sections/messages/message-forms/messageSend.tsx
@@ -31,103 +31,101 @@ export default function SendMessage({
   onError: (msg: string) => void;
 }) {
   return (
-    <>
-      <Formik
-        initialValues={{
-          name: '',
-          message: '',
-          submit: null
-        }}
-        validationSchema={Yup.object().shape({
-          name: Yup.string().max(255).required('Name is required'),
-          message: Yup.string().max(255).required('Message is required')
-        })}
-        onSubmit={(values, { setErrors, setSubmitting, setValues, resetForm }) => {
-          console.dir(values);
+    <Formik
+      initialValues={{
+        name: '',
+        message: '',
+        submit: null
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().max(255).required('Name is required'),
+        message: Yup.string().max(255).required('Message is required')
+      })}
+      onSubmit={(values, { setErrors, setSubmitting, setValues, resetForm }) => {
+        console.dir(values);
 
-          axios
-            .post('c/message', { name: values.name, message: values.message, priority })
-            .then((response) => {
-              setSubmitting(false);
-              resetForm({
-                values: {
-                  name: '',
-                  message: '',
-                  submit: null
-                }
-              });
-              onSuccess();
-            })
-            .catch((error) => {
-              console.error(error);
-              setErrors({ name: error.message });
-              setSubmitting(false);
-              onError(error.message);
+        axios
+          .post('c/message', { name: values.name, message: values.message, priority })
+          .then((response) => {
+            setSubmitting(false);
+            resetForm({
+              values: {
+                name: '',
+                message: '',
+                submit: null
+              }
             });
-        }}
-      >
-        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-          <form noValidate onSubmit={handleSubmit}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="name">Sender's Name</InputLabel>
-                  <OutlinedInput
-                    id="sender-name"
-                    type="text"
-                    value={values.name}
-                    name="name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Enter the sender's name"
-                    fullWidth
-                    error={Boolean(touched.name && errors.name)}
-                  />
-                </Stack>
-                {touched.name && errors.name && (
-                  <FormHelperText error id="standard-weight-helper-text-name-message-send">
-                    {errors.name}
-                  </FormHelperText>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="message">Message</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.message && errors.message)}
-                    id="-password-login"
-                    type="text"
-                    value={values.message}
-                    name="message"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Enter a message"
-                  />
-                </Stack>
-                {touched.message && errors.message && (
-                  <FormHelperText error id="standard-weight-helper-text-msg-message-send">
-                    {errors.message}
-                  </FormHelperText>
-                )}
-              </Grid>
-
-              {errors.submit && (
-                <Grid item xs={12}>
-                  <FormHelperText error>{errors.submit}</FormHelperText>
-                </Grid>
+            onSuccess();
+          })
+          .catch((error) => {
+            console.error(error);
+            setErrors({ name: error.message });
+            setSubmitting(false);
+            onError(error.message);
+          });
+      }}
+    >
+      {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+        <form noValidate onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Stack spacing={1}>
+                <InputLabel htmlFor="name">Sender's Name</InputLabel>
+                <OutlinedInput
+                  id="sender-name"
+                  type="text"
+                  value={values.name}
+                  name="name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Enter the sender's name"
+                  fullWidth
+                  error={Boolean(touched.name && errors.name)}
+                />
+              </Stack>
+              {touched.name && errors.name && (
+                <FormHelperText error id="standard-weight-helper-text-name-message-send">
+                  {errors.name}
+                </FormHelperText>
               )}
-              <Grid item xs={12}>
-                <AnimateButton>
-                  <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
-                    SEND!
-                  </Button>
-                </AnimateButton>
-              </Grid>
             </Grid>
-          </form>
-        )}
-      </Formik>
-    </>
+            <Grid item xs={12}>
+              <Stack spacing={1}>
+                <InputLabel htmlFor="message">Message</InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  error={Boolean(touched.message && errors.message)}
+                  id="-password-login"
+                  type="text"
+                  value={values.message}
+                  name="message"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Enter a message"
+                />
+              </Stack>
+              {touched.message && errors.message && (
+                <FormHelperText error id="standard-weight-helper-text-msg-message-send">
+                  {errors.message}
+                </FormHelperText>
+              )}
+            </Grid>
+
+            {errors.submit && (
+              <Grid item xs={12}>
+                <FormHelperText error>{errors.submit}</FormHelperText>
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <AnimateButton>
+                <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                  SEND!
+                </Button>
+              </AnimateButton>
+            </Grid>
+          </Grid>
+        </form>
+      )}
+    </Formik>
   );
 }
