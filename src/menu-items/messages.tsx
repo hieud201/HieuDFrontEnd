19cba887d1a61347e5132f448e5d695035a18cf7
hieud201 <hieud201@uw.edu>
2024-11-26 21:11:35 -0800

    added book page

--- src/menu-items/messages.tsx
+++ src/menu-items/messages.tsx
@@ -3,14 +3,19 @@ import { FormattedMessage } from 'react-intl';
 
 // assets
 import MessageOutlined from '@ant-design/icons/MessageOutlined';
+import BookOutlined from '@ant-design/icons/BookOutlined';
 import EmailIcon from '@mui/icons-material/Email';
 import SendIcon from '@mui/icons-material/Send';
+import DeleteOutlined from '@ant-design/icons/DeleteOutlined';
+import PlusOutlined from '@ant-design/icons/PlusOutlined';
+import SearchOutlined from '@ant-design/icons/SearchOutlined';
+
 
 // type
 import { NavItemType } from 'types/menu';
 
 // icons
-const icons = { MessageOutlined, EmailIcon, SendIcon };
+const icons = { MessageOutlined, EmailIcon, SendIcon, BookOutlined, DeleteOutlined, PlusOutlined, SearchOutlined };
 
 // ==============================|| MENU ITEMS - PAGES ||============================== //
 
@@ -38,6 +43,35 @@ const pages: NavItemType = {
           type: 'item',
           url: '/messages/list',
           icon: icons.EmailIcon
+        }
+      ]
+    },
+    {
+      id: 'books',
+      title: <FormattedMessage id="books" />,
+      type: 'collapse',
+      icon: icons.BookOutlined,
+      children: [
+        {
+          id: 'view-book',
+          title: <FormattedMessage id="view-book" />,
+          type: 'item',
+          url: '/books/view',
+          icon: icons.SearchOutlined
+        },
+        {
+          id: 'add-book',
+          title: <FormattedMessage id="add-book" />,
+          type: 'item',
+          url: '/books/add',
+          icon: icons.PlusOutlined
+        },
+        {
+          id: 'delete-book',
+          title: <FormattedMessage id="delete-book" />,
+          type: 'item',
+          url: '/books/delete',
+          icon: icons.DeleteOutlined
         }
       ]
     }
