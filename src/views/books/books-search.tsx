d57f7e2ce44afe03f517dc10a029fa545e729d36
hieud201 <hieud201@uw.edu>
2024-12-04 14:05:58 -0800

    fixed rating search, added year search

--- src/views/books/books-search.tsx
+++ src/views/books/books-search.tsx
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App3.css';
 import BookDisplay from './components/bookDisplay';
 import { Box, Pagination } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
 import { useSession } from 'next-auth/react';
 
 interface Book {
@@ -15,17 +14,17 @@ interface Book {
   publication: number;
   title: string;
   ratings: {
-      average: number;
-      count: number;
-      rating_1: string;
-      rating_2: string;
-      rating_3: string;
-      rating_4: string;
-      rating_5: string;
+    average: number;
+    count: number;
+    rating_1: string;
+    rating_2: string;
+    rating_3: string;
+    rating_4: string;
+    rating_5: string;
   };
   icons: {
-      large: string;
-      small: string;
+    large: string;
+    small: string;
   };
 }
 
@@ -34,6 +33,7 @@ interface SearchCriteria {
   author: string;
   title: string;
   rating: string;
+  year: string;
 }
 
 const App3: React.FC = () => {
@@ -47,7 +47,8 @@ const App3: React.FC = () => {
     isbn: '',
     author: '',
     title: '',
-    rating: ''
+    rating: '',
+    year: ''
   });
 
   // Fetch initial books
@@ -57,17 +58,17 @@ const App3: React.FC = () => {
         const limit = 100;
         const offset = 0;
         const response = await fetch(`http://localhost:4000/retrieve/retrieveBooks?limit=${limit}&offset=${offset}`, {
-          method: "GET",
+          method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${session?.token?.accessToken}`
+            Authorization: `Bearer ${session?.token?.accessToken}`
           }
         });
-        
+
         if (!response.ok) {
           throw new Error('Failed to fetch books');
         }
-        
+
         const data = await response.json();
 
         const books = data.books.filter((book: Book) => book.isbn13.length >= 13);
@@ -102,18 +103,21 @@ const App3: React.FC = () => {
         queryParam = `title=${searchCriteria.title}`;
       } else if (searchCriteria.rating) {
         endpoint += 'retrieveRating';
-        queryParam = `rating=${searchCriteria.rating}`;
+        queryParam = `minRating=${parseFloat(searchCriteria.rating) - 0.05}&maxRating=${parseFloat(searchCriteria.rating) + 0.05}`;
+      } else if (searchCriteria.year) {
+        endpoint += 'retrieveYear';
+        queryParam = `startYear=${searchCriteria.year}&endYear=${searchCriteria.year}`;
       } else {
         // If no search criteria, fetch all books
         return;
       }
 
       const response = await fetch(`${endpoint}?${queryParam}`, {
-        method: "GET",
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${session.token.accessToken}`,
-        },
+          Authorization: `Bearer ${session.token.accessToken}`
+        }
       });
 
       if (!response.ok) {
@@ -136,10 +140,11 @@ const App3: React.FC = () => {
       isbn: '',
       author: '',
       title: '',
-      rating: ''
+      rating: '',
+      year: ''
     };
     updatedCriteria[field] = value;
-    
+
     setSearchCriteria(updatedCriteria);
   };
 
@@ -149,7 +154,8 @@ const App3: React.FC = () => {
       isbn: '',
       author: '',
       title: '',
-      rating: ''
+      rating: '',
+      year: ''
     });
   };
 
@@ -163,20 +169,11 @@ const App3: React.FC = () => {
     <div>
       <Card>
         <Card.Body>
-          <div className="website-header">
-            <h1>BOOKHUB</h1>
-          </div>
-        </Card.Body>
-      </Card>
-
-      <Card>
-        <p><SearchIcon /> Search Book</p>
-        <Card.Body>
           <div className="search-information">
             <p className="search-text">ISBN</p>
-            <input 
-              className="search-input" 
-              type="text" 
+            <input
+              className="search-input"
+              type="text"
               value={searchCriteria.isbn}
               onChange={(e) => handleInputChange('isbn', e.target.value)}
               disabled={searchCriteria.isbn === '' && Object.values(searchCriteria).some((val, idx) => idx !== 0 && val !== '')}
@@ -185,9 +182,9 @@ const App3: React.FC = () => {
 
           <div className="search-information">
             <p className="search-text">Author</p>
-            <input 
-              className="search-input" 
-              type="text" 
+            <input
+              className="search-input"
+              type="text"
               value={searchCriteria.author}
               onChange={(e) => handleInputChange('author', e.target.value)}
               disabled={searchCriteria.author === '' && Object.values(searchCriteria).some((val, idx) => idx !== 1 && val !== '')}
@@ -195,55 +192,51 @@ const App3: React.FC = () => {
           </div>
 
           <div className="search-information">
-            <p className="search-text">Rating</p>
-            <input 
-              className="search-input" 
-              type="text" 
-              value={searchCriteria.rating}
-              onChange={(e) => handleInputChange('rating', e.target.value)}
-              disabled={searchCriteria.rating === '' && Object.values(searchCriteria).some((val, idx) => idx !== 2 && val !== '')}
+            <p className="search-text">Title</p>
+            <input
+              className="search-input"
+              type="text"
+              value={searchCriteria.title}
+              onChange={(e) => handleInputChange('title', e.target.value)}
+              disabled={searchCriteria.title === '' && Object.values(searchCriteria).some((val, idx) => idx !== 2 && val !== '')}
             />
           </div>
 
           <div className="search-information">
-            <p className="search-text">Title</p>
-            <input              
-              className="search-input" 
-              type="text" 
-              value={searchCriteria.title}
-              onChange={(e) => handleInputChange('title', e.target.value)}
-              disabled={searchCriteria.title === '' && Object.values(searchCriteria).some((val, idx) => idx !== 3 && val !== '')}
+            <p className="search-text">Rating</p>
+            <input
+              className="search-input"
+              type="text"
+              value={searchCriteria.rating}
+              onChange={(e) => handleInputChange('rating', e.target.value)}
+              disabled={searchCriteria.rating === '' && Object.values(searchCriteria).some((val, idx) => idx !== 3 && val !== '')}
             />
           </div>
 
-          <button 
-            className="book-search-button" 
-            onClick={handleSearch}
-          >
+          <div className="search-information">
+            <p className="search-text">Year</p>
+            <input
+              className="search-input"
+              type="text"
+              value={searchCriteria.year}
+              onChange={(e) => handleInputChange('year', e.target.value)}
+              disabled={searchCriteria.year === '' && Object.values(searchCriteria).some((val, idx) => idx !== 4 && val !== '')}
+            />
+          </div>
+
+          <button className="book-search-button" onClick={handleSearch}>
             Book Search
           </button>
-          <button 
-            className="reset-button"
-            onClick={handleReset}
-          >
+          <button className="reset-button" onClick={handleReset}>
             Reset
           </button>
         </Card.Body>
       </Card>
 
-      <BookDisplay 
-        books={fetchedBooks} 
-        currentPage={currentPage} 
-        booksPerPage={booksPerPage} 
-      />
+      <BookDisplay books={fetchedBooks} currentPage={currentPage} booksPerPage={booksPerPage} />
 
       <Box display="flex" justifyContent="center" sx={{ margin: '20px 0px' }}>
-        <Pagination 
-          count={totalPage} 
-          page={currentPage} 
-          onChange={handlePageChange} 
-          color="primary" 
-        />
+        <Pagination count={totalPage} page={currentPage} onChange={handlePageChange} color="primary" />
       </Box>
     </div>
   );
