01cd3add25b0ee80f38a7c6ca94c81ddb6f14670
Khobaib Zafar <Kzafar@infoblox.com>
2024-12-05 16:22:16 -0800

    Added add Page

---
+++ src/app/(dashboard)/books/add/page.tsx
@@ -0,0 +1,7 @@
+import BookAdd from 'views/books/book-add';
+
+// ==============================|| PAGE ||============================== //
+
+export default function SignInPage() {
+  return <BookAdd />;
+}
