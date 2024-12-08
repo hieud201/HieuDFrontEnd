ed1ce7950a7b25723cf26786fa69b45b412a77ed
KhobitheCode <146394323+KhobitheCode@users.noreply.github.com>
2024-12-08 13:11:57 -0800

    Remove messages section from dashboard navigation

--- src/menu-items/pages.tsx
+++ src/menu-items/pages.tsx
@@ -2,20 +2,16 @@
 import { FormattedMessage } from 'react-intl';
 
 // assets
-import MessageOutlined from '@ant-design/icons/MessageOutlined';
 import BookOutlined from '@ant-design/icons/BookOutlined';
-import EmailIcon from '@mui/icons-material/Email';
-import SendIcon from '@mui/icons-material/Send';
 import PlusOutlined from '@ant-design/icons/PlusOutlined';
 import SearchOutlined from '@ant-design/icons/SearchOutlined';
 import EyeOutlined from '@ant-design/icons/EyeOutlined';
-
 
 // type
 import { NavItemType } from 'types/menu';
 
 // icons
-const icons = { MessageOutlined, EmailIcon, SendIcon, BookOutlined, PlusOutlined, SearchOutlined, EyeOutlined };
+const icons = { BookOutlined, PlusOutlined, SearchOutlined, EyeOutlined };
 
 // ==============================|| MENU ITEMS - PAGES ||============================== //
 
@@ -24,28 +20,6 @@ const pages: NavItemType = {
   title: <FormattedMessage id="pages" />,
   type: 'group',
   children: [
-    {
-      id: 'messages',
-      title: <FormattedMessage id="messages" />,
-      type: 'collapse',
-      icon: icons.MessageOutlined,
-      children: [
-        {
-          id: 'send-message',
-          title: <FormattedMessage id="send-message" />,
-          type: 'item',
-          url: '/messages/send',
-          icon: icons.SendIcon
-        },
-        {
-          id: 'view-messages',
-          title: <FormattedMessage id="view-messages" />,
-          type: 'item',
-          url: '/messages/list',
-          icon: icons.EmailIcon
-        }
-      ]
-    },
     {
       id: 'books',
       title: <FormattedMessage id="books" />,
