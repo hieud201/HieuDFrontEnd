b6e99ee70fb6daca44659ce129db25e0169a67a9
KhobitheCode <146394323+KhobitheCode@users.noreply.github.com>
2024-12-08 13:46:41 -0800

    Fix search page component name

--- src/app/(dashboard)/books/search/page.tsx
+++ src/app/(dashboard)/books/search/page.tsx
@@ -2,6 +2,6 @@ import App3 from 'views/books/books-search';
 
 // ==============================|| PAGE ||============================== //
 
-export default function SignInPage() {
+export default function BookSearchPage() {
   return <App3 />;
 }
